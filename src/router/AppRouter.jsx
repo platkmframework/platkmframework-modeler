99d89b16e7adce0f0617e9a4d0ef4baf4cced69b
eiglesias-soaint <eiglesias.soaint@gmail.com>
2024-10-14 17:26:29 +0200

    added other diagram

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,10 @@
 import { Link, Navigate, Outlet, Route, Routes, useNavigate } from 'react-router-dom';           
  
 import { ModelerMenu } from '../modules/menu/ModelerMenu';
-import { NavigationMainPage } from '../modules/navigation/NavigationMainPage';
+import { EntitiesDiagramModeler } from '../modules/modeler/entities/EntitiesDiagramModeler';
+import { NavigationDiagramModeler } from '../modules/modeler/navigation/NavigationDiagramModeler';
+import { BpmnDiagramModeler } from '../modules/modeler/bpmn/BpmnDiagramModeler';
+import { SequenceDiagramModeler } from '../modules/modeler/sequence/SequenceDiagramModeler';
 
 export const AppRouter = () => {
 
@@ -11,9 +14,10 @@ export const AppRouter = () => {
           <ModelerMenu /> 
         </div>
         <Routes> 
-          <Route path="/modeler/sequence" element={<NavigationMainPage/>}/> 
-          <Route path="/modeler/bpmn" element={<NavigationMainPage/>}/> 
-          <Route path="/modeler/enitty-relation" element={<NavigationMainPage/>}/> 
+          <Route path="/modeler/sequence" element={<SequenceDiagramModeler/>}/> 
+          <Route path="/modeler/bpmn" element={<BpmnDiagramModeler/>}/> 
+          <Route path="/modeler/navigator" element={<NavigationDiagramModeler/>}/> 
+          <Route path="/modeler/entities" element={<EntitiesDiagramModeler/>}/> 
         </Routes>
     </>  
   )
